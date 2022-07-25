6a4834eb0e8a74b5b3e0daadc116e576b70ac240
duythanhnguyen <106386241+popcari@users.noreply.github.com>
2022-07-25 13:52:16 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,7 @@
 </template>
 
 <script>
-// import PageThree from './screen/Page3/Page3.vue';
-// import PageTwo from "./screen/Page2/Page2.vue"
-// import PageOne from './screen/Page1/Page1.vue';
+
 
 export default {
 	name: "App",
